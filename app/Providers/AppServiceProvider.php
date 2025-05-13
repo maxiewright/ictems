d7e78851403a9a015684549541455e87fb5de792
Maxie Wright <maxiewright@gmail.com>
2025-05-12 20:38:38 -0400

    chore: configure basic defaults in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentShield\Facades\FilamentShield;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict(! app()->isProduction());
+
+        Model::automaticallyEagerLoadRelationships();
+
+        Date::use(CarbonImmutable::class);
+
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        FilamentShield::prohibitDestructiveCommands(app()->isProduction());
     }
 }
